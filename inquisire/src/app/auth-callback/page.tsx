606d6212166237699e0ea759d4c9e49420de6133
Ankit Yadav <89292263+Theminacious@users.noreply.github.com>
2024-10-12 17:35:21 +0000

    "Added @tanstack/react-query, @trpc/client, @trpc/next, @trpc/react-query, @trpc/server, and zod dependencies to package.json and package-lock.json. Updated auth-callback/page.tsx to

--- inquisire/src/app/auth-callback/page.tsx
+++ inquisire/src/app/auth-callback/page.tsx
@@ -1,9 +1,13 @@
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
-const Page=()=>{
+const Page=async()=>{
     const router = useRouter()
     const searchParams  =useSearchParams()
     const origin  = searchParams.get('orign')
+
+
+    const {data} = trpc.test.useQuery()
 
     
 }
